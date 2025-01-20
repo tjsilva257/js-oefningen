94d71b608bf93d5d38409f334351ecb43b88d2cc
tjsilva257 <163521712+tjsilva257@users.noreply.github.com>
2025-01-20 15:09:23 +0100

    update

---
+++ oefening 3/js/main.js
@@ -0,0 +1,8 @@
+const input = document.querySelector(".input");
+const bttn = document.querySelector(".bttn");
+const ul = document.querySelector(".ul");
+
+bttn.addEventListener("click", () =>{
+    const task = input.value.trim();
+
+})
